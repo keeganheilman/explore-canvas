73f492276e1672f88123a27fa8d401ae0467d51f
keeganheilman <keeganheilman@gmail.com>
2024-08-31 15:15:10 -0700

    create Player class

--- 01-multiroom-platformer/index.js
+++ 01-multiroom-platformer/index.js
@@ -4,26 +4,45 @@ const c = canvas.getContext('2d')
 canvas.width = 64 * 16 //1024
 canvas.height = 64 * 9 //576
 
-const playerHeight = 100
-let y = 100
-let floor = y + playerHeight
+class Player {
+    constructor() {
+        this.position = {
+            x: 100,
+            y: 100
+        }
+
+        this.width = 100
+        this.height = 100
+        this.sides = {
+            bottom: this.position.y + this.height
+        }
+    }
+
+    draw() {
+    // draw player rectangle
+    c.fillStyle = 'red'
+    c.fillRect(this.position.x, this.position.y, this.width, this.height)
+    }
+
+    update() {
+        // check whether player is at canvas floor
+        if (this.sides.bottom < canvas.height) {
+            this.position.y++
+            this.sides.bottom = this.position.y + this.height
+        }
+    }
+}
+
+
+const player = new Player()
 
 function animate() {
     window.requestAnimationFrame(animate)
-    
     //set canvas background
     c.fillStyle = 'white'
     c.fillRect(0, 0, canvas.width, canvas.height)
-
-    // set player rectangle
-    c.fillStyle = 'red'
-    c.fillRect(100,y,100, playerHeight)
-
-    // check whether player is at canvas floor
-    if (floor < canvas.height) {
-        y++
-        floor = y + playerHeight
-    }
+    player.draw()
+    player.update()
 }
 
 animate()
