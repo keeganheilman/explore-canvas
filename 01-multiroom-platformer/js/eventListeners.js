b6c476b7337074d9d9c5ba1750e9a27a654a7bea
keeganheilman <keeganheilman@gmail.com>
2024-09-07 14:12:34 -0700

    correct hitbox for door collision

--- 01-multiroom-platformer/js/eventListeners.js
+++ 01-multiroom-platformer/js/eventListeners.js
@@ -10,8 +10,8 @@ window.addEventListener('keydown', (event)=> {
             for (let i =0; i < doors.length; i++) {
                 const door = doors[i]
                 if (
-                    player.hitbox.position.x <= door.position.x + door.width &&
-                    player.hitbox.position.x + player.hitbox.width >= door.position.x &&
+                    player.hitbox.position.x + player.hitbox.width <= door.position.x + door.width &&
+                    player.hitbox.position.x >= door.position.x &&
                     player.hitbox.position.y + player.hitbox.height >= door.position.y &&
                     player.hitbox.position.y <= door.position.y + door.height
                 ) {
